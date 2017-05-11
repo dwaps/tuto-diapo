0cd15369cfb005e594a68edd1526d5d5cf4e5069
Michael Cornillon <contact@dwaps.fr>
2017-05-11 21:22:18 +0200

    Implémentation code boutons play et stop

--- js/diaporama.js
+++ js/diaporama.js
@@ -1,34 +1,80 @@
-var diaporama = document.querySelector("#diaporama");
-var diapoOn = false; // Etat du diaporama (ON/OFF)
+const NB_SLIDES = 8;
+const SPEED = 2000; // en ms
+
+var
+  diaporama = document.querySelector("#diaporama")
+  bts = diaporama.querySelectorAll("div"),
+
+  btPrevious = bts[0],
+  btPlay = bts[1],
+  btStop = bts[2],
+  btNext = bts[3]
+;
+
+var diapoOn = false, firstStart = true; // Etat du diaporama (ON/OFF) et état de démarrage
 var cpt = 0; // compteur de slides
+
+
+// GESTION EVENEMENTS
+
+btPlay.addEventListener(
+  'click',
+  function()
+  {
+    diapoOn = true;
+    startDiaporama();
+    console.log("Play !");
+  },
+  false
+);
+
+btStop.addEventListener(
+  'click',
+  function()
+  {
+    diapoOn = false;
+    console.log("Stop !");
+  },
+  false
+);
+
+
+// FONCTIONS
 
 function startDiaporama()
 {
-  if(diapoOn)
-  {
-    setTimeout(
-      function()
+  setTimeout(
+    function()
+    {
+      if(diapoOn)
       {
-        cpt++;
+        // Si pression sur play au premier démarrage
+        // cela signifie que slide1 est déjà affiché
+        // => il faut empêcher qu'il ne reste affiché 2 fois plus longtemps que les autres images
+        if(firstStart) cpt += 2;
+        else cpt++;
+
+        // A ce niveau, nous sommes sûr de ne plus être au premier démarrage du diaporama
+        firstStart = false;
+
+        console.log("Affichage : slide ", cpt);
+      
         // On change l'image du diaporama grâce au compteur de slides
         diaporama.style.backgroundImage = "url('../img/slide"+cpt+".jpg')";
 
+        // On réinitialise le compteur de slides
+        // si on a atteint la dernière image
+        if(cpt == NB_SLIDES) cpt = 0;
+
         // On appelle à nouveau la méthode (récursion)
         startDiaporama();
-      },
-      2000
-    );
-  }
-
-  // On réinitialise le compteur de slides
-  // si on a atteint la dernière image
-  if(cpt == 8) cpt = 0;
+      }
+    },
+    SPEED
+  );
 }
 
 function stopDiaporama()
 {
   diapoOn = false;
 }
-
-diapoOn = true;
-startDiaporama();
